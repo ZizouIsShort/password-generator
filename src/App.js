5359bf00c3a6d6a85660612dcb2e7797a6cf48bb
Ziyan Solkar <ziyansolkar@gmail.com>
2024-07-13 20:47:28 +0400

    finally fixed it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-export function App() {
+function App() {
   return (
     <>
       <div className="bg-blue-800">
-          <p>Hi</p>
+          <p className="text-2xl">Hi</p>
       </div>
     </>
   );
 }
+
+export default App
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
-
+import App from "./App";
+import './index.css';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
-root.render(<h1>Hello, world</h1>);
+root.render(
+
+        <App />
+
+);
